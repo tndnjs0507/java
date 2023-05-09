2533736cfba26331f182e176d271b84c61cf0059
05-09 <05-09@DESKTOP-3Q0ND47>
2023-05-09 18:11:38 +0900

    5월 9일 수업

--- j0509/src/j0509/C0509_12.java
+++ j0509/src/j0509/C0509_12.java
@@ -10,7 +10,7 @@ public class C0509_12 {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("문자를 입력하세요.");
 		ch = scan.next().charAt(0);
-		String result = (ch>='a'&&ch<='z')?"영문 소문자입니다.":"영문소문자가 아닙니다.";
+		String result = (ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')?"영문입니다.":"영문자가 아닙니다.";
 		System.out.println(result);
 
 	}
